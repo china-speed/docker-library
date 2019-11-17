9673527df9d1e6e4598773f54c186687debba83b
sink <sinkcup@gmail.com>
2019-11-17 15:09:09 +0800

    fix: create repo 404

--- split.sh
+++ split.sh
@@ -2,12 +2,11 @@
 set -e
 
 rm -rf .subsplit
-main_repo_url="$(git config --get remote.origin.url)"
 git subsplit init "$(git config --get remote.origin.url)"
 git subsplit update
 for d in */ ; do
   dir_name=${d%/}
-  repo_url=${main_repo_url//\.git/-$dir_name}
+  repo_url="china-speed/docker-library-$dir_name"
   desc='[READ ONLY] auto split from china-speed/docker-library'
   hub create -d "$desc" "$repo_url"
   git subsplit publish \
