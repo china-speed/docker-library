9a283566b71586836f5c443795e9f6f2cca1e008
sinkcup <sinkcup@gmail.com>
2019-11-17 21:47:08 +0800

    new: custom domain

--- build-docs.sh
+++ build-docs.sh
@@ -3,6 +3,7 @@
 rm -rf docs
 mkdir -p docs
 cp README.md docs/
+echo "china-speed.org" >docs/CNAME
 echo "site_name: '中国速度（为开发者加速）'
 repo_url: https://github.com/china-speed/docker-library
 edit_uri: edit/master/
